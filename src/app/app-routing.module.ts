5dc9ee01c66467893e91f1499730e5089631d0ae
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-09 12:16:31 -0300

    added route to dragons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListDragonsComponent } from './components/dragons/list-dragons/list-dragons.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dragons',  component: ListDragonsComponent},
+  { path: '', component: ListDragonsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
