ba172a8b798377f660d3538c97e9428ccaacd377
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-09 12:16:13 -0300

    new folder

---
+++ src/app/components/dragons/list-dragons/list-dragons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-dragons',
+  templateUrl: './list-dragons.component.html',
+  styleUrls: ['./list-dragons.component.sass']
+})
+export class ListDragonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
