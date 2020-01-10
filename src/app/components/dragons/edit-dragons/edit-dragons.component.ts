7c3ebf136893ec4d09bb3b201c5aa3fa0f6afe40
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-10 10:33:22 -0300

    new component

---
+++ src/app/components/dragons/edit-dragons/edit-dragons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-dragons',
+  templateUrl: './edit-dragons.component.html',
+  styleUrls: ['./edit-dragons.component.sass']
+})
+export class EditDragonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
