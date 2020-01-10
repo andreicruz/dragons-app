be12737f0978aa3b63ba3dd8390ae162ededa9e4
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-10 13:37:20 -0300

    new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ListDragonsComponent } from './components/dragons/list-dragons/list-dragons.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditDragonsComponent } from './components/dragons/edit-dragons/edit-dragons.component';
+
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -25,8 +29,12 @@ import { MatDialogModule } from '@angular/material/dialog';
     FontAwesomeModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent],
