7b8d2ad367e1182c088b4b6e260012ac0d025e58
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-10 13:38:08 -0300

    changes in url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 };
 
 export const api = {
-  url: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
+  url: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon/'
 }
