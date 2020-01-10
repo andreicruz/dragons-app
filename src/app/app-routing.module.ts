4cebed4a957b0a63e495ff18f0babc266e5b1545
andrei.cruz <andrei_cruz97@hotmail.com>
2020-01-10 19:46:56 -0300

    new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListDragonsComponent } from './components/dragons/list-dragons/list-dragons.component';
+import { AboutDragonComponent } from './components/dragons/about-dragon/about-dragon.component';
 
 
 const routes: Routes = [
-  {path: 'dragons',  component: ListDragonsComponent},
+  { path: 'dragons',  component: ListDragonsComponent },
+  { path: 'dragon/:id', component: AboutDragonComponent },
   { path: '', component: ListDragonsComponent }
 ];
 
